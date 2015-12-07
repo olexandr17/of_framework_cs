9b79b070ddfded20439851bdcfbcd54deb906c3f
www.olexandr@gmail.com <17of0185>
2015-12-07 11:27:45 +0200

    Initial commit

---
+++ Common/Net/GMail.cs
@@ -0,0 +1,25 @@
+﻿using System.Net;
+using System.Net.Mail;
+
+namespace OFClassLibrary.Common.Net
+{
+    public class GMail
+    {
+        public static void SendFrom(string fromAddress, string fromPassword, string toAddress, string subject, string body)
+        {
+            var smtp = new SmtpClient
+            {
+                Host = "smtp.gmail.com",
+                Port = 587,
+                EnableSsl = true,
+                DeliveryMethod = SmtpDeliveryMethod.Network,
+                UseDefaultCredentials = false,
+                Credentials = new NetworkCredential(fromAddress, fromPassword)
+            };
+            using (var message = new MailMessage(fromAddress, toAddress) { Subject = subject, Body = body })
+            {
+                smtp.Send(message);
+            }
+        }
+    }
+}
---
+++ Frameworks/WinForms/AutorunHelper.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.Win32;
+using System.Windows.Forms;
+
+namespace OFClassLibrary.Frameworks.WinForms
+{
+    public class AutorunHelper
+    {
+        public static bool IsStartupItem(string appName)
+        {
+            using (var rkApp = Registry.CurrentUser.OpenSubKey(@"SOFTWARE\Microsoft\Windows\CurrentVersion\Run", true))
+            {
+                return rkApp.GetValue(appName) != null;
+            }
+        }
+
+        public static void AddToAutorun(string appName)
+        {
+            using(var rkApp = Registry.CurrentUser.OpenSubKey(@"SOFTWARE\Microsoft\Windows\CurrentVersion\Run", true))
+            {
+                if (!IsStartupItem(appName))
+                    rkApp.SetValue(appName, Application.ExecutablePath.ToString());
+            }
+        }
+
+        public static void DeleteFromAutorun(string appName)
+        {
+            using (var rkApp = Registry.CurrentUser.OpenSubKey(@"SOFTWARE\Microsoft\Windows\CurrentVersion\Run", true))
+            {
+                if (IsStartupItem(appName))
+                    rkApp.DeleteValue(appName, false);
+            }
+        }
+    }
+}
